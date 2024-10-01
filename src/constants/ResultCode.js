90f53e6aab1bc9c817b1ff88aa9a64f568ed9c85
k1v4z <addnguyen32gmail.com>
2024-10-01 20:16:56 +0700

    define results code for http response

---
+++ src/constants/ResultCode.js
@@ -0,0 +1,11 @@
+const resultCodes = {
+    register: {
+        success: 101,
+        fail: 102,
+        invalid: 103,
+        error: 104,
+        userExisted: 105
+    }
+}
+
+module.exports = resultCodes
