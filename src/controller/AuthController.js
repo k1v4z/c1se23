844d5bcf0a41341ef8b8ed341714660779e82cb7
k1v4z <addnguyen32gmail.com>
2024-10-01 20:07:58 +0700

    Call auth service

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,7 +1,12 @@
+const AuthRepository = require("../repository/AuthRepository");
+const AuthService = require("../service/AuthService");
+
 module.exports = new class AuthController {
-    signUp(req, res){
-        res.status(200).json({
-            message: "Test Auth Controller"
-        })
+    async signUp(req, res) {
+        const authBody = req.body
+        const authService = new AuthService(new AuthRepository());
+        const signUpState = await authService.signUp(authBody);
+
+        res.status(200).json(signUpState)
     }
 }
