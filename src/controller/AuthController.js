9960061071166fd3ba20b092b6e2f51f2e1e2573
k1v4z <addnguyen32gmail.com>
2024-10-02 23:43:08 +0700

    update controller

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,20 +1,27 @@
 const AuthService = require("../service/AuthService");
-const UserSchema = require("../schemas/UserSchema");
 const container = require("../container/container");
-
+const resultCodes = require("../constants/ResultCode");
 
 module.exports = new class AuthController {
     async signUp(req, res) {
         const authBody = req.body
         const authService = new AuthService(container); //inject container
         const signUpState = await authService.signUp(authBody);
-        return res.status(signUpState == 101 ? 201 : 200).json(signUpState)
+        return res.status(signUpState.code == resultCodes.register.success ? 201 : 200).json(signUpState)
     }
 
-    async login() {
+    async login(req, res) {
         const authBody = req.body
-        if (!UserSchema.validateAuthUser(authBody).success) {
-            return res.status(200).json(UserSchema.validateAuthUser(authBody).error)
+        const authService = new AuthService(container)
+        const login = await authService.login(authBody)
+        if (login.accessToken) {
+            res.cookie('accessToken', login.accessToken, {
+                signed: true,
+                httpOnly: true,
+                maxAge: 30 * 60 * 1000
+            })
         }
+
+        return res.status(login.code == 101 ? 201 : 200).json(login)
     }
 }
