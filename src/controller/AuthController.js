32e2fef023aa7de547f4f3aa355c97ae378aadfd
k1v4z <addnguyen32gmail.com>
2024-10-02 14:04:10 +0700

    inject container

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,12 +1,20 @@
-const AuthRepository = require("../repository/AuthRepository");
 const AuthService = require("../service/AuthService");
+const UserSchema = require("../schemas/UserSchema");
+const container = require("../container/container");
+
 
 module.exports = new class AuthController {
     async signUp(req, res) {
         const authBody = req.body
-        const authService = new AuthService(new AuthRepository());
+        const authService = new AuthService(container); //inject container
         const signUpState = await authService.signUp(authBody);
+        return res.status(signUpState == 101 ? 201 : 200).json(signUpState)
+    }
 
-        res.status(200).json(signUpState)
+    async login() {
+        const authBody = req.body
+        if (!UserSchema.validateAuthUser(authBody).success) {
+            return res.status(200).json(UserSchema.validateAuthUser(authBody).error)
+        }
     }
 }
