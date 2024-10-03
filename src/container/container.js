372d7d853a60ebe7de034cc277ce7533d5a7ec64
k1v4z <addnguyen32gmail.com>
2024-10-03 13:51:12 +0700

    refactor code

--- src/container/container.js
+++ src/container/container.js
@@ -1,10 +1,14 @@
-const AuthRepository = require("../repository/AuthRepository");
-const UserRepository = require("../repository/UserRepository");
 const RepositoryContainer = require("./RepositoryContainer");
 
 //init container 
 const container = new RepositoryContainer()
+
+//User Repo
+const UserRepository = require("../repository/UserRepository");
 container.register('userRepository', new UserRepository())
+
+//Auth Repo
+const AuthRepository = require("../repository/AuthRepository");
 container.register('authRepository', new AuthRepository())
 
 module.exports = container  
