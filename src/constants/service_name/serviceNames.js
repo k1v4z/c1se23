09fe4738e99789b05e1477051c2a8251e0966c21
k1v4z <addnguyen32gmail.com>
2024-10-08 11:53:05 +0700

    define more service name

--- src/constants/service_name/serviceNames.js
+++ src/constants/service_name/serviceNames.js
@@ -2,5 +2,8 @@ module.exports = {
     AUTH_SERVICE: 'authService',
     USER_SERVICE: 'userService',
     TOKEN_SERVICE: 'tokenService',
-    PASSWORD_HASHING_SERVICE: 'passwordHashingService'
+    PASSWORD_HASHING_SERVICE: 'passwordHashingService',
+    PLAN_SERVICE: 'planService',
+    KIND_SERVICE: 'kindService',
+    DATE_PLAN_SERVICE: 'datePlanService'
 }
