5c2164ebfcf26e84c7a8aba26be3366d13d51e0d
k1v4z <addnguyen32gmail.com>
2024-10-08 11:49:37 +0700

    define plan code response

---
+++ src/constants/http_response/planCode.js
@@ -0,0 +1,10 @@
+const planCodes = {
+    create: {
+        success: "CREATE01",
+        fail: "CREATE02",
+        invalid: "CREATE03",
+        error: "CREATE04",
+    }
+}
+
+module.exports = planCodes
