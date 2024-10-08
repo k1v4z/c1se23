55ae281cf3a76f959a65f18cf495da5e35302a7e
k1v4z <addnguyen32gmail.com>
2024-10-08 11:55:15 +0700

    define create plan method

--- src/controller/PlanController.js
+++ src/controller/PlanController.js
@@ -1,7 +1,34 @@
+const containerNames = require("../constants/container_name/containerNames")
+const planCodes = require("../constants/http_response/planCode")
+const serviceNames = require("../constants/service_name/serviceNames")
+const appContainer = require("../container/registration/containerRegistration")
+
 module.exports = new class PlanController {
-    async createPlan(req, res) {
-        return res.status(200).json({
-            message: "Create plan"
+    constructor() {
+        const serviceContainer = appContainer.get(containerNames.SERVICE_CONTAINER)
+        this.planService = serviceContainer.get(serviceNames.PLAN_SERVICE)
+    }
+
+    createPlan = async (req, res) => {
+        const planBody = req.body
+        
+        const newPlan = await this.planService.createPlan(planBody)
+        //console.log(newPlan.error);
+
+        if (newPlan.statusCode == planCodes.create.invalid) {
+            return res.status(400).json({
+                message: "Data invalid",
+                err: newPlan.error
+            })
+        } else if (newPlan.statusCode == planCodes.create.success) {
+            return res.status(201).json({
+                message: "Create Plan Successful",
+                newPlan
+            })
+        }
+
+        return res.status(500).json({
+            message: "Having trouble when processing"
         })
     }
 }
