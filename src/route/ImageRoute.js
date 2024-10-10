55fa80f58e63c1da2ef5c318fbca84e67b7b0ded
k1v4z <addnguyen32gmail.com>
2024-10-10 08:59:18 +0700

    update route

---
+++ src/route/ImageRoute.js
@@ -0,0 +1,13 @@
+const upload = require("../config/multer");
+const ImageController = require("../controller/ImageController");
+const BaseRoute = require("./BaseRoute");
+
+module.exports = new class ImageRoute extends BaseRoute{
+    constructor(){
+        super()
+    }
+
+    initRoutes(){
+        this.router.post('/upload',upload.array('images', 10) ,ImageController.upload)
+    }
+}
--- src/route/PlanRoute.js
+++ src/route/PlanRoute.js
@@ -2,12 +2,15 @@ const PlanController = require("../controller/PlanController")
 const AuthMiddleware = require("../middleware/AuthMiddleware")
 const BaseRoute = require("./BaseRoute")
 
-module.exports = new class PlanRoute extends BaseRoute{
-    constructor(){
+module.exports = new class PlanRoute extends BaseRoute {
+    constructor() {
         super()
     }
 
-    initRoutes(){
-        this.router.post('/create-plan',AuthMiddleware.authenticateUser,PlanController.createPlan)
+    initRoutes() {
+        this.router.post('/create-plan', AuthMiddleware.authenticateUser, PlanController.createPlan)
+        this.router.get('/get-plan', AuthMiddleware.authenticateUser, PlanController.getPlan)
+        this.router.delete('/delete-plan/:id', AuthMiddleware.authenticateUser, PlanController.deletePlan)
+        this.router.put('/edit-plan/:id', AuthMiddleware.authenticateUser, PlanController.editPlan)
     }
 }
