a8684cfd78b20adb5a8a2d4a2aebe93279f13548
k1v4z <addnguyen32gmail.com>
2024-10-10 08:53:13 +0700

    define get detele update constants

--- src/constants/http_response/planCode.js
+++ src/constants/http_response/planCode.js
@@ -1,9 +1,24 @@
 const planCodes = {
     create: {
-        success: "CREATE01",
-        fail: "CREATE02",
-        invalid: "CREATE03",
-        error: "CREATE04",
+        success: "CREATE-PLAN-01",
+        fail: "CREATE-PLAN-02",
+        invalid: "CREATE-PLAN-03",
+        error: "CREATE-PLAN-04",
+    },
+    get: {
+        success: "GET-PLAN-01",
+        fail: "GET-PLAN-02",
+        error: "GET-PLAN-03"
+    },
+    delete: {
+        success: "DEL-PLAN-01",
+        fail: "DEL-PLAN-02",
+        error: "DEL-PLAN-03"
+    },
+    update: {
+        success: "UP-PLAN-01",
+        fail: "UP-PLAN-02",
+        error: "UP-PLAN-03"
     }
 }
 
