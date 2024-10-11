3100881a0f62188f80eb41722a16bacb023e7ecf
k1v4z <addnguyen32gmail.com>
2024-10-11 13:47:08 +0700

    update constructor

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -1,17 +1,19 @@
+const containerNames = require("../constants/container_name/containerNames")
+const serviceNames = require("../constants/service_name/serviceNames")
 const appContainer = require("../container/registration/containerRegistration")
-const TokenService = require("../service/TokenService")
 
 module.exports = new class AuthMiddleware {
-    constructor(){
-        this.tokenService = new TokenService(appContainer)
+    constructor() {
+        const serviceContainer = appContainer.get(containerNames.SERVICE_CONTAINER)
+        this.tokenService = serviceContainer.get(serviceNames.TOKEN_SERVICE)
     }
 
     authenticateUser = (req, res, next) => {
         const accessToken = req.signedCookies.accessToken
-        try{
+        try {
             const payload = this.tokenService.verifyToken(accessToken)
             next()
-        }catch(err){
+        } catch (err) {
             console.log(err);
             return res.status(401).json({
                 error: "Access token is missing or invalid"
