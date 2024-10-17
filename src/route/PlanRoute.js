7bed7b6de6499b2379d8298f4508f11db417beb3
k1v4z <addnguyen32gmail.com>
2024-10-17 13:42:19 +0700

    using dynamic routing for get plan

--- src/route/PlanRoute.js
+++ src/route/PlanRoute.js
@@ -9,7 +9,7 @@ module.exports = new class PlanRoute extends BaseRoute {
 
     initRoutes() {
         this.router.post('/create-plan', AuthMiddleware.authenticateUser, PlanController.createPlan)
-        this.router.get('/get-plan', AuthMiddleware.authenticateUser, PlanController.getPlan)
+        this.router.get('/plan/:id', AuthMiddleware.authenticateUser, PlanController.getPlan)
         this.router.delete('/delete-plan/:id', AuthMiddleware.authenticateUser, PlanController.deletePlan)
         this.router.put('/edit-plan/:id', AuthMiddleware.authenticateUser, PlanController.editPlan)
     }
