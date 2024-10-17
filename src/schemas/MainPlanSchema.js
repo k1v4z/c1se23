2cef4893118408104a50e40801107f261d10d3c6
k1v4z <addnguyen32gmail.com>
2024-10-17 13:43:55 +0700

    modify plan validate

--- src/schemas/MainPlanSchema.js
+++ src/schemas/MainPlanSchema.js
@@ -4,16 +4,13 @@ const iso8601DateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{1,3})?(?:Z
 module.exports = new class MainPlanSchema {
     getActivitySchema() {
         const activitySchema = z.object({
-            name: z.string(),
-            image_url: z.string(),
-            start_time: z.string().refine((val) => iso8601DateRegex.test(val), {
+            activity_location_id: z.string(),
+            start_date: z.string().refine((val) => iso8601DateRegex.test(val), {
                 message: "Invalid ISO 8601 date format"
             }),
-            end_time: z.string().refine((val) => iso8601DateRegex.test(val), {
+            end_date: z.string().refine((val) => iso8601DateRegex.test(val), {
                 message: "Invalid ISO 8601 date format"
             }),
-            longitude: z.number(),
-            latitude: z.number()
         })
 
         return activitySchema
@@ -26,13 +23,12 @@ module.exports = new class MainPlanSchema {
             date: z.string().refine((val) => iso8601DateRegex.test(val), {
                 message: "Invalid ISO 8601 date format"
             }),
-            location: z.string(),
-            image_url: z.string(),
             transportation: z.string(),
             money: z.number(),
             state: z.enum(["pending", "confirmed", "cancelled"]), // if there are specific states
             have_children: z.boolean(),
             kind_name: z.string(),
+            province_name: z.string(),
             activities: z.array(this.getActivitySchema()), //One Plan have many activities
         });
 
