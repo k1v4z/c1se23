df4b96863a3f2fb446d9b1834b9893e986a25d30
k1v4z <addnguyen32gmail.com>
2024-10-17 13:32:12 +0700

    init province service

---
+++ src/service/ProvinceService.js
@@ -0,0 +1,10 @@
+module.exports = class ProvinceService {
+    constructor(provinceRepository) {
+        this.provinceRepository = provinceRepository
+    }
+
+    async getProvinceIdByName(provinceName) {
+        const provinceId = await this.provinceRepository.getProvinceIdByName(provinceName)
+        return provinceId
+    }
+}
