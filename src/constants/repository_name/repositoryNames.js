427656f93616ab1b99c7aa5d8d49f61fe6dcc910
k1v4z <addnguyen32gmail.com>
2024-10-22 13:58:13 +0700

    define repo constant

--- src/constants/repository_name/repositoryNames.js
+++ src/constants/repository_name/repositoryNames.js
@@ -3,5 +3,6 @@ module.exports = {
     AUTH_REPOSITORY: 'authRepository',
     PLAN_REPOSITORY: 'planRepository',
     KIND_REPOSITORY: 'kindRepository',
-    PROVINCE_REPOSITORY: 'provinceRepository'
+    PROVINCE_REPOSITORY: 'provinceRepository',
+    ACTIVITY_LOCATION_REPOSITORY: 'activityLocationRepository'
 }
