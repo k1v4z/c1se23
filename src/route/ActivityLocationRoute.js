e320680d41d2f09a472ed3bbcba297dd96337f73
k1v4z <addnguyen32gmail.com>
2024-10-22 13:54:38 +0700

    init location route

---
+++ src/route/ActivityLocationRoute.js
@@ -0,0 +1,15 @@
+const ActivityLocationController = require("../controller/ActivityLocationController")
+const BaseRoute = require("./BaseRoute")
+
+module.exports = new class ActivityLocationRoute extends BaseRoute {
+    constructor() {
+        super()
+    }
+
+    initRoutes() {
+        this.router.get('/activity-locations', ActivityLocationController.getActivityLocation)
+        this.router.post('/activity-location', ActivityLocationController.createActivityLocation)
+        this.router.delete('/activity-location/:id', ActivityLocationController.deleteActivityLocation)
+        this.router.patch('/activity-location/:id', ActivityLocationController.editActivityLocation)
+    }
+}
